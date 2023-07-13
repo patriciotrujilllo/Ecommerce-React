6548cdbb11f373c2ff60eebfec99c48802c7173e
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-13 11:21:25 -0400

    Create: services for products

---
+++ src/services/productServices.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+
+export const getAllProducts = () =>{
+    return (
+        axios.get('https://fakestoreapi.com/products')
+            .then(res=> res.data)
+    )
+    
+}
