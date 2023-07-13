701dc84fb8a197cf5885554311cc288d8d3d6089
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-13 11:23:02 -0400

    Create: card of product, category,title,price

---
+++ src/component/products/card.js
@@ -0,0 +1,41 @@
+//import './card.css'
+//import './cardContain.css'
+import '../../styles.css'
+
+export const Cards = ({props}) =>{
+    return (
+        <>
+    
+        <div className='item'>
+            <figure>
+                <img src={props.image} alt="" />
+            </figure>
+            
+            <div className='info-product'>
+                <h2>{props.category}</h2>
+                <h4>{props.title}</h4>
+                
+            
+                <span>Price : ${props.price}</span>
+                <button type="button">Agregar producto</button>
+                
+            </div>
+        </div>
+        </>
+    )
+    }
+
+/*
+{
+"id": 1,
+"title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+"price": 109.95,
+"description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+"category": "men's clothing",
+"image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+"rating": {
+"rate": 3.9,
+"count": 120
+}
+}
+*/
