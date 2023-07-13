4ef1b328f3c7a7e0c1abbd36636690248a1101bd
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-13 11:24:52 -0400

    Add: pri cipal view, show all product

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-
+import { getAllProducts } from "./services/productServices"
+import { useState,useEffect } from "react"
+import { Cards } from "./component/products/card"
+import './styles.css'
 function App() {
 
+  const [products,setProducts] = useState([])
+
+  useEffect(()=>{
+
+    getAllProducts()
+    .then(res=>{
+      setProducts(res)
+    })
+
+  },[])
+  
   return (
-    <h1>hello</h1>
+    <div className="container">
+      <div className="item-container">
+        {products.map(product => (
+            <Cards key={product.id} props={product}/>
+        ))} 
+
+      </div>
+    </div>
+    
+      
+    
   )
   }
 
