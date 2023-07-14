2aeeb2a00bbd38882d970ae8f90e1df0835f6e6b
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-13 21:56:48 -0400

    it's not necesary to send or recive the prop

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,10 @@
 import {Filters} from './Filters.js'
 
-export const Header = ({setFilters}) =>{
+export const Header = () =>{
     return (
         <header>
             <h1>React Fake Store</h1>
-            <Filters setFilters={setFilters}/>
+            <Filters/>
         </header>
     )
 }
