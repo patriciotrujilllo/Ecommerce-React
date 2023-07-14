d1ebc41ca2278b8d735ed411f99234a960d0a4d9
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-13 21:00:47 -0400

    Wraps the components that need the context

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {FiltersProvider} from './context/filters'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <FiltersProvider>
     <App />
-  </React.StrictMode>
+  </FiltersProvider>
 );
