72bee62f28e267072fe9b3f5635c6465e32672bc
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-21 17:54:17 -0400

    Remove double container

--- src/component/products/Mapeo.js
+++ src/component/products/Mapeo.js
@@ -2,13 +2,13 @@ import { Cards } from "./card.js"
 
 export const Mapeo = ({filtrado}) =>{
     return (
-        <div className="container">
+        
             <div className="item-container">
                 {filtrado.map(product => (
                     <Cards key={product.id} props={product}/>
                 ))} 
 
             </div>
-        </div>
+        
     )
 }
