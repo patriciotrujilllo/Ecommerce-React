c01cdd281cde89e93cbdb628fc72b81740954bc5
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-29 13:45:17 -0400

    Add nav

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,16 @@
 import {Cart} from './Cart'
+import {BiUserCircle} from 'react-icons/bi'
 export const Header = () =>{
     return (
         <header class='header'>
             <h1>Fake Store</h1>
-            <input type="text" />
-            <Cart />
+            <nav className='nav'>
+                <input type="text" />
+                <div className='icons'>
+                    <BiUserCircle size='2rem'/>
+                    <Cart />
+                </div>
+            </nav>
         </header>
     )
 }
