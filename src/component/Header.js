7ee602a9b272411b098e022b490ec1a764323116
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-29 14:41:00 -0400

    Add handleSeach

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,28 @@
 import {Cart} from './Cart'
 import {BiUserCircle} from 'react-icons/bi'
+import Form from 'react-bootstrap/Form';
+import {useFilters} from '../hooks/useFilters'
+
+
 export const Header = () =>{
+
+    const {setFilters} = useFilters()
+
+    const handleClickSeach = e =>{
+        e.preventDefault()
+        setFilters(prevState=>({
+            ...prevState,
+            title: e.target.value
+        }))
+    }
     return (
         <header class='header'>
             <h1>Fake Store</h1>
             <nav className='nav'>
-                <input type="text" />
+                <Form>
+                    <Form.Label></Form.Label>
+                    <Form.Control type="text" onChange={handleClickSeach}/>
+                </Form>
                 <div className='icons'>
                     <BiUserCircle size='2rem'/>
                     <Cart />
