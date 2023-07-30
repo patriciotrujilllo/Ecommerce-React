6c15f18c5cd0a3d86c6f2b1288cf2076702c1cc7
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-29 20:16:39 -0400

    Add provide of Cart

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {FiltersProvider} from './context/filters'
+import { CartProvider } from "./context/cart"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <FiltersProvider>
-    <App />
-  </FiltersProvider>
+  <CartProvider>
+    <FiltersProvider>
+      <App />
+    </FiltersProvider>
+  </CartProvider>
+  
 );
