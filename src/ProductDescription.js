cda0df0d195235fb9d5b4de415f01fd70d01ab72
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-30 17:18:38 -0400

    Add image, title,price,description and be able to add the product to the cart

--- src/ProductDescription.js
+++ src/ProductDescription.js
@@ -1,11 +1,16 @@
 import { useParams } from "react-router-dom"
 import './productDescription.css'
 import { Header } from "./component/Header"
+import { useCart } from "./hooks/useCart"
+import { useState } from "react"
 
 export const ProductDescription = ({products}) =>{
 
+    const [quantity,setQuantity] = useState(1)
+    const {addToCart} = useCart()
     const {id} = useParams()
     const product = products? products.find(product=>product.id===parseInt(id)): false
+    // const productInCart = cart? cart.find(product=>product.id===parseInt(id)): false
     return(
         product? 
         <main className="main-description">
@@ -13,9 +18,26 @@ export const ProductDescription = ({products}) =>{
             <figure className="figure-description">
                 <img src={product.image} alt="" />
             </figure>
-            <div className="price-description"></div>
-            <div className="product-description"></div>
-            <div className="product-add"></div>
+            <div className="title-price-description">
+                <div className="title">{product.title}</div>
+                <div className="price-description">PRECIO:${product.price}</div>
+            </div>
+            <div className="product-description">
+                {product.description}
+            </div>
+            <div className="product-add">
+                <div className="buttons">
+                    <div className="quantity">
+                    <button onClick={()=>setQuantity(prevState=>prevState>1? prevState-1:prevState)}>-</button>
+                        <div className="number">
+                            {quantity}
+                        </div>
+                    <button onClick={()=>setQuantity(prevState=>prevState+1)}>+</button>
+                    </div>
+                    <button className="Add-cart" onClick={()=>addToCart(product)}>Añadir al carrito</button>
+                    
+                </div>
+            </div>
         </main> : <div>Loading...</div>
     )
 }
