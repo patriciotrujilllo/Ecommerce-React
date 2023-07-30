805b23099f06e008e43c02cf61966f9640c8caf1
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-29 20:16:09 -0400

    Create a componente for main page

---
+++ src/Product.js
@@ -0,0 +1,35 @@
+import { getAllProducts } from "./services/productServices"
+import { useState,useEffect} from "react"
+import { Mapeo } from "./component/products/Mapeo.js"
+import { Header } from "./component/Header.js"
+import './styles.css'
+import { useFilters } from "./hooks/useFilters"
+import { Filters } from "./component/Filters"
+
+export function Products() {
+
+    const [products,setProducts] = useState([])
+
+    useEffect(()=>{
+
+    getAllProducts()
+    .then(res=>{
+        setProducts(res)
+    })
+
+    },[])
+
+    const {filterProducts} = useFilters(products)
+
+    const filtrado = filterProducts(products)
+
+    return (
+
+        <div className="container">
+        <Header/>
+        <Filters/>
+        <Mapeo filtrado={filtrado}/>
+        </div>
+    
+    )
+    }
