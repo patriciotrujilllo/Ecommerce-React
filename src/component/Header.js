278cd46d973010b894b0d3267924068e9fffc9ab
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-31 14:51:29 -0400

    Add navigate:  se redirige al buscar un producto, add Link to home, se cambio handleChange por handleKeyDown

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,27 +2,36 @@ import {Cart} from './Cart'
 import {BiUserCircle} from 'react-icons/bi'
 import Form from 'react-bootstrap/Form';
 import {useFilters} from '../hooks/useFilters'
+import { Link,useNavigate  } from 'react-router-dom';
 import './Header.css'
 
 
 export const Header = () =>{
 
     const {setFilters} = useFilters()
+    const navigate = useNavigate ()
 
-    const handleClickSeach = e =>{
-        e.preventDefault()
-        setFilters(prevState=>({
+    const handleKeyDown = e =>{
+        if (e.key === 'Enter') {
+            e.preventDefault()
+            setFilters(prevState=>({
             ...prevState,
             title: e.target.value
         }))
+        navigate('/')
+        }
+        
     }
     return (
         <header className='header'>
-            <h1>Fake Store</h1>
+            <Link to='/'>
+                <h1>Fake Store</h1>
+            </Link>
+            
             <nav className='nav'>
                 <Form>
                     <Form.Label></Form.Label>
-                    <Form.Control type="text" onChange={handleClickSeach}/>
+                    <Form.Control type="text"  onKeyDown={handleKeyDown}/>
                 </Form>
                 <div className='icons'>
                     <BiUserCircle size='2rem'/>
